fd4916e34609b459fc6dbc95427b343a9aa4351b
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2024-09-21 12:27:38 +0600

    Test git commit -m Test

--- index.js
+++ index.js
@@ -16,27 +16,13 @@ import { connectDB } from "./db/connectDB.js";
 dotenv.config();
 const app = express();
 
-const corsConfig = {
-  origin: ["http://localhost:5173"],
-  credentials: true,
-};
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    const allowedOrigins = ["http://localhost:5173"];
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsConfig));
-app.use(cors(corsOptions));
-app.options("*", cors(corsConfig));
+// CORS configuration to allow all origins
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    credentials: true, // Allow credentials if needed
+  })
+);
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -45,21 +31,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api", usersRoutes);
-
 app.use("/api/bookings", bookingRoutes);
-
 app.use("/api/payment", payments);
-
 app.use("/api/rooms", roomRoutes);
-
 app.use("/api/blogs", blogRoutes);
-
 app.use("/api/category", categoryRoutes);
+
 app.get("/", (req, res) => {
   res.send({ message: "Welcome to the room" });
-});
-app.get("/api/dev", (req, res) => {
-  res.send({ message: "Welcome to the api dev" });
 });
 
 app.listen(PORT, () => {
